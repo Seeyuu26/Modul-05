e976320eba3d775cb2d1c99fdfa73efb793bc3f9
Seeyu <ansyahrusdi649@gmail.com>
2025-09-24 07:15:38 +0700

    Modul08

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'home.dart';
+import 'tujuan.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,75 +12,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
+
     return MaterialApp(
-      title: 'Row and Column',
-      home: Scaffold(body: MusikPage()),
-    );
-  }
-}
-
-class MusikPage extends StatelessWidget {
-  const MusikPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // Tambahkan backgroundColor untuk latar belakang gelap
-      backgroundColor: Colors.grey[800],
-      body: Center(
-        child: Text(
-          "Pemutar Musik",
-          // Ubah warna teks menjadi putih agar kontras dengan latar gelap
-          style: TextStyle(fontSize: 20, color: Colors.white),
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0081c9)),
+        useMaterial3: true,
       ),
-      bottomNavigationBar: buildControlBar(),
+      initialRoute: '/', // rute awal aplikasi
+      routes: {
+        Tujuan.routeName: (context) => const Tujuan(),
+        '/': (context) => const Home(),
+      },
     );
   }
-}
-
-Widget buildControlBar() {
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-    // Ubah warna container menjadi sedikit lebih terang dari background
-    color: Colors.grey[900],
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        // Widget children tetap sama
-        Expanded(
-          child: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.shuffle, color: Colors.white),
-          ),
-        ),
-        Expanded(
-          child: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.skip_previous, color: Colors.white),
-          ),
-        ),
-        Flexible(
-          flex: 2,
-          fit: FlexFit.tight,
-          child: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.play_circle_fill, color: Colors.white, size: 70),
-          ),
-        ),
-        Expanded(
-          child: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.skip_next, color: Colors.white),
-          ),
-        ),
-        Expanded(
-          child: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.repeat, color: Colors.white),
-          ),
-        ),
-      ],
-    ),
-  );
 }
